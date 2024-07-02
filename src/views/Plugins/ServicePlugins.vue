a94d3ee2ec95d39a42187de375724cd1a18b9cce
yiuman <415481084@qq.com>
2024-07-02 22:38:45 +0800

    style: 调整插件列样式

--- src/views/Plugins/ServicePlugins.vue
+++ src/views/Plugins/ServicePlugins.vue
@@ -2,6 +2,7 @@
 import { useServiceStore } from '@/stores/service'
 import { type FileItem } from '@arco-design/web-vue'
 import serviceApi from '@/api/service'
+import type { Service } from '@/types/flow'
 
 const serviceStore = useServiceStore();
 
@@ -10,30 +11,39 @@ async function uploadJar(fileList: FileItem[]): Promise<void> {
     await serviceStore.initData();
 }
 
+function getContainer(): HTMLElement {
+  console.warn(' document.getElementById(\'pluginsContainer\') as HTMLElement', document.getElementById('pluginsContainer') as HTMLElement)
+  return document.getElementById('pluginsContainer') as HTMLElement
+}
+
+const selectedPlugin = ref()
+const [modalVisible, toggleModalVisible] = useToggle(false)
+
+function selectPlugin(serviceItem: Service) {
+  selectedPlugin.value = serviceItem
+  toggleModalVisible()
+}
 </script>
 
 <template>
-    <div class="plugins-container">
+  <div class="plugins-container" id="pluginsContainer">
         <div class="plugins-box">
           <ACard class="plugin-card">
             <AUpload draggable @change="uploadJar" />
           </ACard>
           <ACard class="plugin-card" hoverable v-for="serviceItem in serviceStore.services" :key="serviceItem.id">
-                <template #cover>
-                    <div class="cover-box">
-                        <div class="cover">
-                            <AImage v-if="serviceItem.avatar" :preview="false" :width="120" :height="120"
-                                :src="serviceItem.avatar" />
-                            <AAvatar v-else shape="square" :size="120">{{ serviceItem.name }}
-                            </AAvatar>
-                        </div>
-                    </div>
-                </template>
-                <div class="plugins-title">{{ serviceItem.name }}</div>
+            <div class="cover-box" @click="()=>selectPlugin(serviceItem)">
+              <div class="cover">
+                <AImage v-if="serviceItem.avatar" :preview="false" :width="120" :height="120"
+                        :src="serviceItem.avatar" />
+                <AAvatar v-else shape="square" :size="120">{{ serviceItem.name }}
+                </AAvatar>
+              </div>
+            </div>
+            <div class="plugins-title">{{ serviceItem.name }}</div>
             </ACard>
         </div>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
@@ -50,7 +60,7 @@ async function uploadJar(fileList: FileItem[]): Promise<void> {
     }
   }
   :deep(.arco-card-body){
-    padding: 10px;
+    padding: 8px;
   }
 }
 
@@ -63,21 +73,16 @@ async function uploadJar(fileList: FileItem[]): Promise<void> {
         border-radius: 5px;
     }
 
-    .cover-box {
-        padding: 10px;
-        width: calc(100% - 20px);
-        height: calc(100% - 20px)
-    }
-
     .cover {
       background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='50.41' height='87' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M25.3 87L12.74 65.25m0 14.5h-25.12m75.18 0H37.68M33.5 87l25.28-43.5m-50.23 29l4.19 7.25L16.92 87h-33.48m33.48 0h16.75-8.37zM8.55 72.5L16.92 58m50.06 29h-83.54m79.53-50.75L50.4 14.5M37.85 65.24L50.41 43.5m0 29l12.56-21.75m-50.24-14.5h25.12zM33.66 29l4.2 7.25 4.18 7.25M33.67 58H16.92l-4.18-7.25M-8.2 72.5l20.92-36.25L33.66 0m25.12 72.5H42.04l-4.19-7.26L33.67 58l4.18-7.24 4.19-7.25M33.67 29l8.37-14.5h16.74m0 29H8.38m29.47 7.25H12.74M50.4 43.5L37.85 21.75m-.17 58L25.12 58M12.73 36.25L.18 14.5M0 43.5l-12.55-21.75M24.95 29l12.9-21.75M12.4 21.75L25.2 0M12.56 7.25h-25.12m75.53 0H37.85M58.78 43.5L33.66 0h33.5m-83.9 0h83.89M33.32 29H16.57l-4.18-7.25-4.2-7.25m.18 29H-8.37M-16.74 0h33.48l-4.18 7.25-4.18 7.25H-8.37m8.38 58l12.73-21.75m-25.3 14.5L0 43.5m-8.37-29l21.1 36.25 20.94 36.24M8.37 72.5H-8.36'  stroke-width='1' stroke='hsla(258.5,59.4%,59.4%,1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>");
         background-color: var(--color-fill-3);
-        padding: 10px;
+      padding: 10px 10px 5px 10px;
         border-radius: 5px;
         text-align: center
     }
 
     .plugins-title {
+      padding-top: 10px;
         text-align: center;
         font-weight: bold;
     }
