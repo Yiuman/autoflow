092cf6b0b32edfbd64d58953258e397cd9a1d2ba
yiumankam <415481084@qq.com>
2024-07-02 18:04:45 +0800

    feat: 增加导入工作流文件创建实现

--- src/api/crud.ts
+++ src/api/crud.ts
@@ -6,26 +6,21 @@ import type { PageParameter, PageRecord } from '@/types/crud'
 export default function createCrudRequest<ENTITY = Record<string, any>, KEY = any>(baseUri: string) {
     return {
         get: async (key: KEY) => {
-            const data = await request.get<ENTITY>(`${baseUri}/${key}`);
-            return Promise.resolve(data);
+            return request.get<ENTITY>(`${baseUri}/${key}`)
         },
         page: async (param: PageParameter = { pageNumber: 1, pageSize: 10 }) => {
             const requestURL = `${baseUri}?${qs.stringify(param, { arrayFormat: 'repeat' })}`
-            const data = await request.get<PageRecord<ENTITY>>(requestURL);
-            return Promise.resolve(data);
+            return request.get<PageRecord<ENTITY>>(requestURL);
         },
         list: async (param?: Record<string, any>) => {
             const requestURL = `${baseUri}/list?${qs.stringify(param, { arrayFormat: 'repeat' })}`
-            const data = await request.get<ENTITY[]>(requestURL)
-            return Promise.resolve(data)
+            return request.get<ENTITY[]>(requestURL)
         },
         save: async (entity: ENTITY) => {
-            const data = await request.post<ENTITY>(baseUri, entity);
-            return Promise.resolve(data);
+            return request.post<ENTITY>(baseUri, entity);
         },
         delete: async (key: KEY) => {
-            await request.delete<void>(`${baseUri}/${key}`);
-            return Promise.resolve();
+            return request.delete<void>(`${baseUri}/${key}`);
         },
     }
 }
--- src/views/Workflow/WorkflowList.vue
+++ src/views/Workflow/WorkflowList.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import workflowApi, { type Workflow, type WorkflowQuery } from '@/api/workflow'
-import { IconMoreVertical, IconSearch, IconTags } from '@arco-design/web-vue/es/icon'
-import { Icon, Modal, Notification } from '@arco-design/web-vue'
+import { IconMoreVertical, IconSearch, IconTags, IconPlus } from '@arco-design/web-vue/es/icon'
+import { Icon, Modal, Notification, type FileItem } from '@arco-design/web-vue'
 import { type PageRecord } from '@/types/crud'
 import { useServiceStore } from '@/stores/service'
 import { format } from 'date-fns'
 import type { Service } from '@/types/flow'
 import TagSelector from '@/components/TagSelector/TagSelector.vue'
 import { useRouter } from 'vue-router'
-
+import { downloadByData } from '@/utils/download'
 const iconfontUrl = new URL('/src/assets/iconfont.js', import.meta.url).href;
 const router = useRouter();
 const IconFont = Icon.addFromIconFontCn({ src: iconfontUrl });
@@ -44,6 +44,13 @@ function modifyWorkflow(workflow: Workflow) {
     toggleCreateBlankFormVisible();
 }
 
+function exportWorkflow(workflow: Workflow) {
+    const jsonStr = JSON.stringify(workflow);
+    downloadByData(new Blob([jsonStr], {
+        type: 'text/plain'
+    }), `${workflow.id}.json`)
+}
+
 function deleteWorkflow(workflow: Workflow) {
     Modal.error({
         title: '确认删除吗?',
@@ -71,8 +78,46 @@ function getWorkflowServices(workflow: Workflow): Service[] {
     if (!workflow || !workflow.pluginIds) {
         return [];
     }
-    console.warn("workflow.pluginIds.map(pluginId => serviceStore.getServiceById(pluginId))",workflow.pluginIds.map(pluginId => serviceStore.getServiceById(pluginId)))
     return workflow.pluginIds.map(pluginId => serviceStore.getServiceById(pluginId));
+}
+
+// -------------- 处理导入json文件创建工作流 --------------
+const [uploadFormVisible, toggleUploadFormVisible] = useToggle(false);
+const uploadExistsed = ref(false);
+const fileItem = ref();
+function uploadWorkflowJson(fileList: FileItem[]) {
+    const reader = new FileReader()
+    fileItem.value = fileList[0]
+    reader.readAsText(fileItem.value.file as Blob)
+    reader.onload = async function () {
+        const workflowJson = reader.result as string
+        const workflow: Workflow = JSON.parse(workflowJson);
+        if (workflow.id) {
+            const storeWorkflow = await workflowApi.get(workflow.id);
+            if (storeWorkflow) {
+                uploadExistsed.value = true;
+                workflowInstance.value = workflow;
+            }
+        } else {
+            await workflowApi.save(workflow);
+            toggleUploadFormVisible();
+            fileItem.value = null;
+        }
+    }
+}
+
+async function saveUploadWorkflow(cover: boolean) {
+    const uploadWorkflow = workflowInstance.value;
+    if (!cover) {
+        delete uploadWorkflow.id;
+    }
+    await workflowApi.save(uploadWorkflow);
+    uploadExistsed.value = false;
+    fileItem.value = null;
+    resetInstance();
+    toggleUploadFormVisible();
+    fetch();
+
 }
 
 
@@ -91,28 +136,29 @@ function getWorkflowServices(workflow: Workflow): Service[] {
 
         </div>
         <div class="workflow-list">
-          <ACard class="workflow-add-card" :bordered="false" hoverable title="创建新的工作流">
-              <div class="workflow-add-btn" @click="() => toggleCreateBlankFormVisible()">
-                  <IconFont type="icon-chuangjian" /> 创建空白工作流
-              </div>
-              <div class="workflow-add-btn">
-                  <IconFont type="icon-template-success-fill" /> 从应用模板创建
-              </div>
-              <div class="workflow-add-btn">
-                  <IconFont type="icon-w_daoru" /> 导入工作流文件创建
-              </div>
-          </ACard>
-          <ACard class="workflow-card-item" hoverable v-for="workflow in currentPageRecord?.records"
+            <ACard class="workflow-add-card" :bordered="false" hoverable title="创建新的工作流">
+                <div class="workflow-add-btn" @click="() => toggleCreateBlankFormVisible()">
+                    <IconFont type="icon-chuangjian" /> 创建空白工作流
+                </div>
+                <div class="workflow-add-btn">
+                    <IconFont type="icon-template-success-fill" /> 从应用模板创建
+                </div>
+                <div class="workflow-add-btn" @click="() => toggleUploadFormVisible()">
+                    <IconFont type="icon-w_daoru" /> 导入工作流文件创建
+                </div>
+            </ACard>
+            <ACard class="workflow-card-item" hoverable v-for="workflow in currentPageRecord?.records"
                 :key="workflow.id">
                 <div>
                     <ADropdown trigger="hover">
-                      <IconMoreVertical size="16" class="workflow-card-operator" />
+                        <IconMoreVertical size="16" class="workflow-card-operator" />
                         <template #content>
-                          <div class="workflow-card-operator-box">
-                            <div class="workflow-card-operator-item" @click="modifyWorkflow(workflow)">编辑</div>
-                            <div class="workflow-card-operator-item"
+                            <div class="workflow-card-operator-box">
+                                <div class="workflow-card-operator-item" @click="modifyWorkflow(workflow)">编辑</div>
+                                <div class="workflow-card-operator-item"
                                     @click="router.push(`/flowdesign?flowId=${workflow.id}`)">编排</div>
-                            <div class="workflow-card-operator-item card-delete-btn"
+                                <div class="workflow-card-operator-item" @click="exportWorkflow(workflow)">导出工作流文件</div>
+                                <div class="workflow-card-operator-item card-delete-btn"
                                     @click="deleteWorkflow(workflow)">删除</div>
                             </div>
 
@@ -134,14 +180,14 @@ function getWorkflowServices(workflow: Workflow): Service[] {
                             </AOverflowList>
                         </ADescriptionsItem>
                         <ADescriptionsItem label="update">
-                          <span style="font-size: 13px">{{ format(workflow.updateTime || 0, 'yyyy-MM-dd HH:mm:ss')
-                            }}</span>
+                            <span style="font-size: 13px">{{ format(workflow.updateTime || 0, 'yyyy-MM-dd HH:mm:ss')
+                                }}</span>
                         </ADescriptionsItem>
                     </ADescriptions>
 
-                  <div class="workflow-card-item-tags" v-if="workflow.tags && workflow.tags.length">
+                    <div class="workflow-card-item-tags" v-if="workflow.tags && workflow.tags.length">
                         <IconTags />
-                    <AOverflowList class="workflow-card-item-tags-value">
+                        <AOverflowList class="workflow-card-item-tags-value">
                             <ATag v-for="tag in workflow.tags" :key="tag.id" color="blue">{{ tag.name }}</ATag>
                         </AOverflowList>
                     </div>
@@ -164,6 +210,36 @@ function getWorkflowServices(workflow: Workflow): Service[] {
                         <TagSelector v-model="workflowInstance.tagIds" allow-create />
                     </AFormItem>
                 </AForm>
+            </AModal>
+            <AModal v-model:visible="uploadFormVisible" modal-class="upload-workflow-modal" @cancel="resetInstance"
+                draggable :footer="false">
+                <template #title>
+                    导入工作流文件创建
+                </template>
+                <AUpload :auto-upload="false" :show-file-list="false" draggable @change="uploadWorkflowJson">
+                    <template #upload-button>
+                        <div class="arco-upload-picture-card">
+                            <div class="arco-upload-picture-card-text" v-if="fileItem">
+                                {{ fileItem.name }}
+                            </div>
+                            <div class="arco-upload-picture-card-text" v-else>
+                                <IconPlus />
+                                <div style="margin-top: 10px; font-weight: 600">
+                                    点击或拖拽文件到此处上传
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                </AUpload>
+                <template v-if="uploadExistsed">
+                    <div class="upload-repeat-alert">
+                        当前导入的工作流已存在，请选择
+                        <AButton size="mini" status="warning" type="outline" @click="() => saveUploadWorkflow(true)">覆盖
+                        </AButton>
+                        或 <AButton size="mini" type="primary" @click="() => saveUploadWorkflow(false)">创建</AButton>
+                        新的工作流
+                    </div>
+                </template>
             </AModal>
         </div>
         <APagination show-total show-jumper show-page-size v-model:current="queryObj.pageNumber"
