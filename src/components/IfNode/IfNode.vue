8a951d01bde48e2e10194d8c608b4984b9bac667
yiumankam <415481084@qq.com>
2024-07-08 18:29:16 +0800

    fix: 修正IF节点的状态判断

--- src/components/IfNode/IfNode.vue
+++ src/components/IfNode/IfNode.vue
@@ -13,26 +13,43 @@ const [avatarNotFound, toggleAvatar] = useToggle(false)
 <template>
   <ServiceNode class="if-node" v-bind="props" :actionClass="'none-action'">
     <template #default>
-      <Handle id="INPUT" type="target" :position="Position.Left" :is-valid-connection="validConnection" />
-      <Handle id="IF_TRUE" type="source" :position="Position.Top" :is-valid-connection="validConnection">
+      <Handle
+        id="INPUT"
+        type="target"
+        :position="Position.Left"
+        :is-valid-connection="validConnection"
+      />
+      <Handle
+        id="IF_TRUE"
+        type="source"
+        :position="Position.Top"
+        :is-valid-connection="validConnection"
+      >
         <div class="true-label">true</div>
       </Handle>
-      <Handle id="IF_FALSE" type="source" :position="Position.Bottom" :is-valid-connection="validConnection">
+      <Handle
+        id="IF_FALSE"
+        type="source"
+        :position="Position.Bottom"
+        :is-valid-connection="validConnection"
+      >
         <div class="false-label">false</div>
       </Handle>
     </template>
     <template #avatar>
       <AAvatar shape="square" :size="68" :class="data.running ? 'node-action' : ''">
-        <div  v-if="avatarNotFound"  class="node-if-label">IF</div>
+        <div v-if="avatarNotFound" class="node-if-label">IF</div>
         <AImage
           v-else
           class="if-avatar-img"
           :width="68"
-          :src="`${VITE_BASE_URL || '/api'}/services/image/${data.serviceId}`" @error="() => toggleAvatar()" />
+          :src="`${VITE_BASE_URL || '/api'}/services/image/${data.serviceId}`"
+          @error="() => toggleAvatar()"
+        />
       </AAvatar>
 
       <div class="node-status-icon" v-if="data.executionData">
-        <IconExclamationCircle class="node-status-error" v-if="data.executionData.error" />
+        <IconExclamationCircle class="node-status-error" v-if="data.executionData[0].error" />
         <IconCheckCircle class="node-status-success" v-else />
       </div>
     </template>
